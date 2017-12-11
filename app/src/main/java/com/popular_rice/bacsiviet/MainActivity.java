52d3323b2f17760ad7d98db6dfe466bc5924ef81
TRANTUAN2603 <TUAN260387@GMAIL.COM>
2017-12-11 08:41:29 +0700

    Initial commit

---
+++ app/src/main/java/com/popular_rice/bacsiviet/MainActivity.java
@@ -0,0 +1,81 @@
+package com.popular_rice.bacsiviet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    //thoi gian tri hoan chuyen trang
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_group:
+                    transaction.replace(R.id.frame_layout, new CommunityFragment()).commit();
+                    return true;
+                case R.id.navigation_help:
+//                    transaction.replace(R.id.frame_layout,new CommunityFragment()).commit();
+                    return true;
+                case R.id.navigation_chat:
+//                    transaction.replace(R.id.frame_layout,new CommunityFragment()).commit();
+                    return true;
+                case R.id.navigation_notifications:
+//                    transaction.replace(R.id.frame_layout,new CommunityFragment()).commit();
+                    return true;
+                case R.id.navigation_menu:
+                    transaction.replace(R.id.frame_layout, new MenuFragment()).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            goLoginScreen();
+        }
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, new CommunityFragment()).commit();
+    }
+
+    private void goLoginScreen() {
+        //tri hoan chuyen trang
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
